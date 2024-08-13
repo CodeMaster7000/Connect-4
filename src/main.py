9aa947205b6950ffe367b167bfaa0be11f204a0d
CodeMaster7000 <CodeMaster7K@gmail.com>
2024-08-13 17:28:51 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -79,7 +79,7 @@ RADIUS = int(SQUARESIZE/2 - 5)
 screen = pygame.display.set_mode(size)
 draw_board(board)
 pygame.display.update()
-myfont = pygame.font.SysFont("monospace", 75)
+myfont = pygame.font.SysFont("monospace", 64)
 
 while not game_over:
  
@@ -107,7 +107,7 @@ while not game_over:
                     drop_piece(board, row, col, 1)
  
                     if winning_move(board, 1):
-                        label = myfont.render("Player 1 wins", 1, YELLOW)
+                        label = myfont.render("Player 1 won!", 1, YELLOW)
                         screen.blit(label, (40,10))
                         game_over = True
  
@@ -120,7 +120,7 @@ while not game_over:
                     drop_piece(board, row, col, 2)
  
                     if winning_move(board, 2):
-                        label = myfont.render("Player 2 wins", 1, YELLOW)
+                        label = myfont.render("Player 2 won!", 1, YELLOW)
                         screen.blit(label, (40,10))
                         game_over = True
  
